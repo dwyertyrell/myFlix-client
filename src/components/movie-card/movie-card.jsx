998b3c62853d6bc078fd0a2b8ab490d19b2310f0
dwyertyrell <Dwyertyrell@gmail.com>
2025-02-27 09:54:42 +0000

    added props validation to React components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,15 @@
+import PropTypes from 'prop-types'
+
 export const MovieCard = ({movie, onMovieClick}) => {
     return <div onClick={()=> {
         onMovieClick(movie);
     }}
     > {movie.title}</div>
 } 
+
+MovieCard.PropTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
